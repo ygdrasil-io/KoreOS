3d2320ea483d5ae1e791ec992520302ee9944eeb
Alexandre Mommers <919982+ygdrasil-io@users.noreply.github.com>
2026-04-29 18:05:22 +0200

    GRA-1: Ajout settings.gradle.kts pour module clang-wrapper

---
+++ shared/clang-wrapper/settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "clang-wrapper"
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
